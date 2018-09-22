e39433c7df5209aa5b3dc0bf51ea96ef6059c329
zhenfeii <zhenfei.huang@gdkyit.com>
2018-09-22 09:13:24 +0800

    新增的测试类
    详细内容：
    新增的测试类
    
    Co-Authored-By: Jiabin Huang <jiabin.huang@biologie.uni-freiburg.de>

---
+++ src/main/java/com/gdkyit/utils/Test.java
@@ -0,0 +1,11 @@
+package com.gdkyit.utils;
+
+/**
+ * Created by Administrator on 9/22/2018.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
